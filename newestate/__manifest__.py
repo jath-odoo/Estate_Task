49026385f23b919d84712948fd702398aa06b570
jath-odoo <jath@odoo.com>
2022-02-04 11:19:24 +0530

    [ADD] add modules

---
+++ newestate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Estate',
+    'application': True,
+    'installble':True,
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/estate_menu.xml',
+        'views/estate_view.xml',
+        'views/template.xml',
+        'wizard/property_view.xml',
+        ],
+    'license':'LGPL-3',
+}
---
+++ newestate/recordrpc.py
@@ -0,0 +1,26 @@
+import xmlrpc.client
+
+
+db = "odoo17"
+username = "admin"
+password = "admin"
+
+common = xmlrpc.client.ServerProxy('http://localhost:8069/xmlrpc/2/common')
+uid = common.authenticate(db, username, password, {})
+
+if uid:
+    print ("Connection Successful")
+
+models = xmlrpc.client.ServerProxy('http://localhost:8069/xmlrpc/2/object')
+
+
+
+to_confrim_ids = models.execute_kw(db, uid, password, 'estate.properties', 'search', [[('name', '=', 'car')]])
+#print ("\n\nto_confrim_id ::: ", to_confrim_ids)
+
+result = models.execute_kw(db, uid, password, 'estate.properties', 'action_sold', [to_confrim_ids])
+
+#result = models.execute_kw(db, uid, password, 'estate.properties', 'search_read', [[], ['name', 'description', 'state']])
+
+print ("\n\nresult is ::: ", result)
+
